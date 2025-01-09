805bd5bed6fe56c8e2161b3b211475386c80f305
Aaron James <ajustinjames@gmail.com>
2025-01-09 08:37:32 -0500

    lighthouse optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  adapter: cloudflare()
+  minify: true
 })
