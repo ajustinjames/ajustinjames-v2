909b655f4239584450e50a1eeafd2c1488bf408e
Aaron James <github@ajustinjames.com>
2025-01-14 01:26:05 -0500

    removing api from website for contact form (#15)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon';
 
-import cloudflare from '@astrojs/cloudflare';
-
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  minify: true,
-  adapter: cloudflare()
+  minify: true
 })
