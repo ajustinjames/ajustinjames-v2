b74d7535a2c795395b3f4050aa3bc9595911966f
Aaron James <github@ajustinjames.com>
2025-01-13 19:49:19 -0500

    adding cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  minify: true
+  minify: true,
+  adapter: cloudflare()
 })
