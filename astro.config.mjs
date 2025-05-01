93d1019ddce3c61d3ec71a5bf3f3105aa94b5ac9
Aaron James <contact@ajustinjames.com>
2025-04-30 20:23:47 -0400

    Add sitemap package (#36)
    * Add sitemap integration and update configuration
    
    - Added `@astrojs/sitemap` integration to `astro.config.mjs` with weekly change frequency and priority settings.
    - Updated `package.json` and `package-lock.json` to include `@astrojs/sitemap` as a dependency.
    - Modified `robots.txt` to include the sitemap URL for better SEO.
    
    This enhances the site's SEO capabilities by providing a structured sitemap for search engines.
    
    * package upgrades

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  site: 'https://ajustinjames.com',
+  integrations: [
+    tailwind(), 
+    icon(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date()
+    })
+  ],
   minify: true
 })
